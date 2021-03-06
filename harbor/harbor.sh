18369a9187f0d40596dcf334ba283a131752de1d
Federico Iezzi <fiezzi@redhat.com>
2021-03-06 21:55:54 +0100

    [harbor] enable SSL

--- harbor/harbor.sh
+++ harbor/harbor.sh
@@ -14,6 +14,18 @@ if [ -d /root/harbor ]; then
     exit 1
 fi
 
+# Verify if the RootCA KEY is available to sign the Harbor PEM
+if [ -f /root/ca.key ]; then
+    echo "Error, missing RootCA Key under /root/ca.key."
+    exit 1
+fi
+
+# Verify if the RootCA CRT is available to sign the Harbor PEM
+if [ -f /root/ca.crt ]; then
+    echo "Error, missing RootCA Key under /root/ca.crt."
+    exit 1
+fi
+
 echo "Welcome to the Harbor installation for CentOS 8 Stream"
 echo "https://github.com/goharbor/harbor"
 
@@ -72,9 +84,59 @@ COMPOSEVERSION=$(curl -s https://github.com/docker/compose/releases/latest/downl
 curl -L "https://github.com/docker/compose/releases/download/$COMPOSEVERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# Create local CA and PEM for Harbor
+echo "[Step 0]: Create Harbor PEM ..."
+_DIR=/root/certs
+mkdir -p /root/certs
+
+# Create OpenSSL Config for the Certificate
+cat > "${_DIR}/openssl.cnf" << EOF
+[req]
+default_bits = 2048
+encrypt_key  = no
+default_md   = sha256
+prompt       = no
+utf8         = yes
+distinguished_name = req_distinguished_name
+req_extensions = v3_req
+
+[req_distinguished_name]
+CN = harbor.ocp4.bm.nfv.io
+
+[v3_req]
+basicConstraints     = CA:FALSE
+subjectKeyIdentifier = hash
+keyUsage             = digitalSignature, keyEncipherment
+extendedKeyUsage     = clientAuth, serverAuth
+subjectAltName       = @alt_names
+
+[alt_names]
+IP.1  = 10.0.11.27
+DNS.1 = harbor.ocp4.bm.nfv.io
+EOF
+
+# Create Certificate Key
+openssl req \
+ -new -newkey rsa:2048 \
+ -keyout "${_DIR}/cert.key" \
+ -out "${_DIR}/cert.csr" \
+ -config "${_DIR}/openssl.cnf"
+
+# Create PEM
+openssl x509 \
+ -req \
+ -days 1825 \
+ -in "${_DIR}/cert.csr" \
+ -extensions v3_req \
+ -extfile "${_DIR}/openssl.cnf" \
+ -CA "/root/ca.crt" \
+ -CAkey "/root/ca.key" \
+ -CAcreateserial \
+ -out "${_DIR}/cert.pem"
+
 # Download and run Harbor installation
 echo "[Step 0]: Install Latest Stable Harbor Release ..."
-mkdir /root/harbor
+mkdir -p /root/harbor
 cd /root/harbor
 HARBORVERSION=$(curl -s https://github.com/goharbor/harbor/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
 curl -s https://api.github.com/repos/goharbor/harbor/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
@@ -82,12 +144,10 @@ tar xvf harbor-online-installer-v$HARBORVERSION.tgz
 cd harbor
 cp harbor.yml.tmpl harbor.yml
 sed -i "s/reg.mydomain.com/$IPorFQDN/g" harbor.yml
-sed -e '/port: 443$/ s/^#*/#/' -i harbor.yml
-sed -e '/https:$/ s/^#*/#/' -i harbor.yml
-sed -e '/\/your\/certificate\/path$/ s/^#*/#/' -i harbor.yml
-sed -e '/\/your\/private\/key\/path$/ s/^#*/#/' -i harbor.yml
+sed -i "s|/your/certificate/path|${_DIR}/cert.pem|g" -i harbor.yml
+sed -i "s|/your/private/key/path|${_DIR}/cert.key|g" -i harbor.yml
 
-./install.sh --with-chartmuseum
+./install.sh --with-notary --with-trivy --with-chartmuseum
 
 # Print default login credentials
 echo "Harbor installation completed!"
